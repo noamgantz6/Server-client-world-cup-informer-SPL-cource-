b1acc239791bec26ab12c6b07dbaab258cd740b3
noamgantz6 <noamgantz6@gmail.com>
2026-07-27 17:24:26 +0300

    Delete 218291391_332932318 directory

--- 218291391_332932318/client/src/StompClient.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include <StompProtocol.h>
-int main(int argc, char *argv[]) {
-	// TODO: implement the STOMP client
-	StompProtocol protocol;
-	protocol.start();
-	return 0;
-}
